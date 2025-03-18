b013f2e4ae92bf790a4537430185520edd81622f
James Wang <james.wang@apricusventures.sg>
2025-03-18 13:48:19 +0800

    Initial Commit

---
+++ CloudAsset/CloudAssetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudAssetApp.swift
+//  CloudAsset
+//
+//  Created by James Wang on 18/3/25.
+//
+
+import SwiftUI
+
+@main
+struct CloudAssetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CloudAssetTests/CloudAssetTests.swift
@@ -0,0 +1,17 @@
+//
+//  CloudAssetTests.swift
+//  CloudAssetTests
+//
+//  Created by James Wang on 18/3/25.
+//
+
+import Testing
+@testable import CloudAsset
+
+struct CloudAssetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
