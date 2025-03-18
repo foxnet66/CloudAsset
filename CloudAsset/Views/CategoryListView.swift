643001987a38a560eb16c0407c6d41f71e7a36a9
James Wang <james.wang@apricusventures.sg>
2025-03-19 00:40:57 +0800

    add discription for icon

--- CloudAsset/Views/CategoryListView.swift
+++ CloudAsset/Views/CategoryListView.swift
@@ -162,38 +162,38 @@ struct CategoryFormView: View {
     }
     
     // 系统图标选择 - 资产管理相关图标
-    private let iconOptions: [String] = [
+    private let iconOptions: [(icon: String, label: String)] = [
         // 电子设备类
-        "laptopcomputer", "desktopcomputer", "tv", "display", "headphones",
-        "phone", "iphone", "ipad", "applewatch", "airpods", "homepod.fill",
-        "gamecontroller", "camera", "video", "printer", "keyboard", "mouse",
+        ("laptopcomputer", "笔记本"), ("desktopcomputer", "台式机"), ("tv", "电视"), ("display", "显示器"), ("headphones", "耳机"),
+        ("phone", "手机"), ("iphone", "iPhone"), ("ipad", "iPad"), ("applewatch", "智能表"), ("airpods", "耳机"), ("homepod.fill", "音箱"),
+        ("gamecontroller", "游戏机"), ("camera", "相机"), ("video", "摄像机"), ("printer", "打印机"), ("keyboard", "键盘"), ("mouse", "鼠标"),
         
         // 家居类
-        "house", "bed.double", "sofa", "chair", "table.furniture", "lamp", 
-        "tv.and.mediabox", "refrigerator", "oven", "washer", "microwave",
+        ("house", "房屋"), ("bed.double", "床"), ("sofa", "沙发"), ("chair", "椅子"), ("table.furniture", "桌子"), ("lamp", "灯"), 
+        ("tv.and.mediabox", "影音"), ("refrigerator", "冰箱"), ("oven", "烤箱"), ("washer", "洗衣机"), ("microwave", "微波炉"),
         
         // 交通工具
-        "car", "bicycle", "airplane", "bus", "tram.fill", "scooter",
+        ("car", "汽车"), ("bicycle", "自行车"), ("airplane", "飞机"), ("bus", "公交车"), ("tram.fill", "电车"), ("scooter", "滑板车"),
         
         // 金融类
-        "creditcard", "creditcard.fill", "banknote", "dollarsign.circle", 
-        "dollarsign.square", "bag", "cart", "tag", "giftcard", "wallet.pass",
+        ("creditcard", "信用卡"), ("creditcard.fill", "信用卡"), ("banknote", "钞票"), ("dollarsign.circle", "货币"), 
+        ("dollarsign.square", "货币"), ("bag", "购物袋"), ("cart", "购物车"), ("tag", "标签"), ("giftcard", "礼品卡"), ("wallet.pass", "钱包"),
         
         // 办公用品
-        "folder", "doc", "book", "newspaper", "briefcase", "case",
-        "paperclip", "ruler", "pencil", "highlighter", "scissors", "paintbrush",
+        ("folder", "文件夹"), ("doc", "文档"), ("book", "书籍"), ("newspaper", "报纸"), ("briefcase", "公文包"), ("case", "箱子"),
+        ("paperclip", "回形针"), ("ruler", "尺子"), ("pencil", "铅笔"), ("highlighter", "荧光笔"), ("scissors", "剪刀"), ("paintbrush", "画笔"),
         
         // 服装/个人物品
-        "tshirt", "backpack", "handbag", "eyeglasses", "watch", "shoe",
+        ("tshirt", "衣服"), ("backpack", "背包"), ("handbag", "手提包"), ("eyeglasses", "眼镜"), ("watch", "手表"), ("shoe", "鞋子"),
         
         // 工具
-        "hammer", "wrench", "screwdriver", "powerplug", "lightbulb", "battery.100",
+        ("hammer", "锤子"), ("wrench", "扳手"), ("screwdriver", "螺丝刀"), ("powerplug", "电源"), ("lightbulb", "灯泡"), ("battery.100", "电池"),
         
         // 收藏品
-        "photo", "film", "guitars", "pianokeys", "book.closed", "medal",
+        ("photo", "照片"), ("film", "胶片"), ("guitars", "吉他"), ("pianokeys", "钢琴"), ("book.closed", "藏书"), ("medal", "奖牌"),
         
         // 珠宝/奢侈品
-        "diamond", "crown", "seal", "wand.and.stars", "gift", "sparkles"
+        ("diamond", "钻石"), ("crown", "皇冠"), ("seal", "印章"), ("wand.and.stars", "魔杖"), ("gift", "礼物"), ("sparkles", "珠宝")
     ]
     
     // 加载现有数据
@@ -250,34 +250,41 @@ struct CategoryFormView: View {
             
             Section(header: Text("图标")) {
                 ScrollView {
-                    LazyVGrid(columns: Array(repeating: GridItem(.adaptive(minimum: 55), spacing: 10), count: 5), spacing: 12) {
+                    LazyVGrid(columns: Array(repeating: GridItem(.adaptive(minimum: 60), spacing: 8), count: 4), spacing: 12) {
                         ForEach(0..<iconOptions.count, id: \.self) { index in
-                            let iconName = iconOptions[index]
+                            let iconData = iconOptions[index]
                             Button {
                                 withAnimation {
-                                    icon = iconName
-                                    print("选择了图标: \(iconName)")
+                                    icon = iconData.icon
+                                    print("选择了图标: \(iconData.icon) - \(iconData.label)")
                                 }
                             } label: {
-                                VStack {
+                                VStack(spacing: 4) {
                                     ZStack {
                                         Circle()
-                                            .fill(icon == iconName ? Color.blue.opacity(0.2) : Color.gray.opacity(0.1))
-                                            .frame(width: 50, height: 50)
+                                            .fill(icon == iconData.icon ? Color.blue.opacity(0.2) : Color.gray.opacity(0.1))
+                                            .frame(width: 48, height: 48)
                                         
-                                        Image(systemName: iconName)
-                                            .font(.system(size: 22))
-                                            .foregroundColor(icon == iconName ? .blue : .primary)
+                                        Image(systemName: iconData.icon)
+                                            .font(.system(size: 20))
+                                            .foregroundColor(icon == iconData.icon ? .blue : .primary)
                                     }
+                                    
+                                    Text(iconData.label)
+                                        .font(.system(size: 10))
+                                        .foregroundColor(icon == iconData.icon ? .blue : .secondary)
+                                        .lineLimit(1)
+                                        .fixedSize()
                                 }
+                                .frame(width: 60, height: 70)
                             }
                             .buttonStyle(BorderlessButtonStyle()) // 阻止点击事件传播
-                            .id("icon_\(iconName)")
+                            .id("icon_\(iconData.icon)")
                         }
                     }
                     .padding(10)
                 }
-                .frame(height: 270)
+                .frame(height: 320)
                 .background(Color.gray.opacity(0.05))
                 .cornerRadius(8)
             }
